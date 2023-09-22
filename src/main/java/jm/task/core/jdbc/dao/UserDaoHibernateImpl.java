45f4cdd0f434e384556d9ab3f9b910209eacc3eb
Chester004 <br_nsd@mail.ru>
2023-09-22 18:06:54 +0300

    Не знал что begin.transaction возвращает Transaction instance

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -4,14 +4,16 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import javax.persistence.EntityNotFoundException;
 import javax.persistence.PersistenceException;
 import javax.persistence.Query;
 import java.util.ArrayList;
 import java.util.List;
+
 public class UserDaoHibernateImpl implements UserDao {
-    SessionFactory sessionFactory = Util.getSessionFactory();
+    private SessionFactory sessionFactory = Util.getSessionFactory();
 
     public UserDaoHibernateImpl() {
 
@@ -25,67 +27,64 @@ public class UserDaoHibernateImpl implements UserDao {
                 + "lastname VARCHAR(20) NOT NULL, "
                 + "age TINYINT(3) NOT NULL, " + "PRIMARY KEY (ID) "
                 + ")";
-        Session session = sessionFactory.openSession();
-        try {
+        try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
             session.createSQLQuery(createTableSQL).executeUpdate();
             session.getTransaction().commit();
         } catch (PersistenceException e) {
             System.err.println("Ошибка создания таблицы");
-        } catch (Exception e) {
-            session.getTransaction().rollback();
         }
     }
 
     @Override
     public void dropUsersTable() {
         String dropSQL = "DROP TABLE `users`";
-        Session session = sessionFactory.openSession();
-        try {
+        try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
             session.createSQLQuery(dropSQL).executeUpdate();
             session.getTransaction().commit();
         } catch (PersistenceException e) {
             System.err.println("Таблица уже удалена");
-        } catch (Exception e) {
-            session.getTransaction().rollback();
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Session session = sessionFactory.openSession();
-        try {
-            session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
             session.save(new User(name, lastName, age));
-            session.getTransaction().commit();
+            transaction.commit();
             System.out.printf("User с именем – %s добавлен в базу данных \n", name);
         } catch (Exception e) {
-            session.getTransaction().rollback();
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = sessionFactory.openSession();
-        try {
-            session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession();) {
+            transaction = session.beginTransaction();
             String hql = "DELETE User WHERE id = :ID";
             Query query = session.createQuery(hql);
             query.setParameter("ID", id);
             query.executeUpdate();
-            session.getTransaction().commit();
+            transaction.commit();
         } catch (EntityNotFoundException e) {
             System.err.printf("Id %d нет в таблице \n", id);
         } catch (Exception e) {
-            session.getTransaction().rollback();
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
     @Override
     public List<User> getAllUsers() {
-        try {
-            Session session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             String hql = "FROM User";
             Query query = session.createQuery(hql);
             List<User> users = query.getResultList();
@@ -98,17 +97,19 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        Session session = sessionFactory.openSession();
-        try {
-            session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
             String hql = "DELETE User ";
             Query query = session.createQuery(hql);
             query.executeUpdate();
-            session.getTransaction().commit();
+            transaction.commit();
         } catch (EntityNotFoundException e) {
             System.err.println("Таблица пуста");
         } catch (Exception e) {
-            session.getTransaction().rollback();
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 }
